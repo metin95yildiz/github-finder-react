37a5d8fd8ade416850bc23ce37bf37a604f7fcd5
metin95yildiz <metin_y1995@hotmail.com>
2022-02-22 21:39:52 +0300

    Created reducers

---
+++ src/store/reducers/profile.js
@@ -0,0 +1,22 @@
+import {
+    COMPLETE_SEARCH_USER,
+    FAIL_SEARCH_USER
+} from "../actions/actionTypes";
+
+const initial = {
+    user: "",
+    repos: "",
+    error: ""
+}
+
+export default function profile(state = initial, action) {
+    switch(action.type) {
+        case COMPLETE_SEARCH_USER: return {
+            ...state,
+            user: action.user,
+            repos: action.repos
+        };
+        case FAIL_SEARCH_USER: return { ...state, error: action.error };
+        default: return state;
+        }
+}
